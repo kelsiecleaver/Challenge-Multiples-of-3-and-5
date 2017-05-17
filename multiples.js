0a68cc2187969f892f809342b9c5ddda628e0d12
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-05-17 08:47:06 -1000

    updated

--- multiples.js
+++ multiples.js
@@ -6,8 +6,8 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
-  for(var i = 0; i <n; i++){
-    if(i % === 0 || i % 5 === 0){
+  for(var i = 0; i < n; i++){
+    if(i % 3 === 0 || i % 5 === 0){
       sum += i;
     }
   }
